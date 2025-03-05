b62f2129fae8c39c0478e704289650e432f4bbcf
Vladislav <senior.vlados97@gmail.com>
2025-03-05 22:17:55 +0300

    implement tcp server and client for file sending

---
+++ TCPClient/TCPClient.cpp
@@ -0,0 +1,109 @@
+﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#include <iostream>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <string>
+#include <fstream>
+#include <thread>
+
+#pragma comment(lib, "ws2_32.lib")
+
+const int BUFFER_SIZE = 4096;
+
+/// <summary>
+/// Отправляет файл на сервер
+/// </summary>
+/// <param name="socket">сокет, по которому будут приходить данные от клиента на сервер</param>
+/// <param name="path">путь к файлу</param>
+void sendFile(SOCKET socket, std::string& path) {
+    std::ifstream file(path, std::ios::binary | std::ios::ate);
+
+    if (!file.is_open()) {
+        std::cout << "Ошибка при открытии файла на чтение" << std::endl;
+        return;
+    }
+
+    // Определим размер файла
+    long fileSize = file.tellg();
+    file.seekg(0, std::ios::beg);
+
+    // Отправим имя файла на сервер
+    std::string fileName = path.substr(path.find_last_of("\\") + 1);
+    send(socket, fileName.c_str(), fileName.size(), 0);
+
+    // Отправим размер файла на сервер
+    std::string fileSizeStr = std::to_string(fileSize);
+    send(socket, fileSizeStr.c_str(), fileSizeStr.size(), 0);
+
+    // Отправим данные файла на сервер
+    char buffer[BUFFER_SIZE];
+
+    while (!file.eof()) {
+        file.read(buffer, BUFFER_SIZE);
+        send(socket, buffer, file.gcount(), 0);
+    }
+
+    file.close();
+
+    // Получим подтверждение об успешной отправке файла
+    char msg[BUFFER_SIZE];
+    int bytesRecived = recv(socket, msg, BUFFER_SIZE, 0);
+
+    if (bytesRecived > 0) {
+        msg[bytesRecived - 1] = '\0';
+        std::cout << "Ответ от сервера: " << msg << std::endl;
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "");
+
+    WSADATA wsaData;
+
+    // Иницифализация WinSock
+    int err = WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+    if (err != 0) {
+        std::cout << "Ошибка при инициализации WinSock." << std::endl;
+        return 1;
+    }
+
+    SOCKET clientSocket;
+
+    // Открытие сокета
+    clientSocket = socket(AF_INET, SOCK_STREAM, 0); // IPv4, TCP-протокол
+
+    if (clientSocket == INVALID_SOCKET) {
+        WSACleanup();
+        std::cout << "Ошибка при открытии сокета" << std::endl;
+        return 1;
+    }
+
+    sockaddr_in serverAddr = { 0 };
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(1234);
+    serverAddr.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+
+    // Подключаемся к серверу
+    err = connect(clientSocket, (sockaddr*)&serverAddr, sizeof(serverAddr));
+
+    if (err == SOCKET_ERROR) {
+        closesocket(clientSocket);
+        WSACleanup();
+        std::cout << "Ошибка при подключении к серверу" << std::endl;
+        return 1;
+    }
+
+    std::string path;
+    std::cout << "Введите путь к файлу: ";
+    std::cin >> path;
+
+    sendFile(clientSocket, path);
+
+    shutdown(clientSocket, SD_BOTH);
+    closesocket(clientSocket);
+    WSACleanup();
+
+    return 0;
+}
---
+++ TCPServer/TCPServer.cpp
@@ -0,0 +1,145 @@
+﻿#include <iostream>
+#include <WinSock2.h>
+#include <string>
+#include <fstream>
+#include <thread>
+
+#pragma comment(lib, "ws2_32.lib")
+
+const int BUFFER_SIZE = 4096;
+
+/// <summary>
+/// Обрабатывает загрузку файлов от клиента
+/// </summary>
+/// <param name="clientSocket">Сокет клиента</param>
+void handleClient(SOCKET clientSocket) {
+    char buffer[BUFFER_SIZE];
+    int bytesRecieved;
+
+    // Получим имя файла
+    bytesRecieved = recv(clientSocket, buffer, BUFFER_SIZE, 0);
+
+    if (bytesRecieved <= 0) {
+        closesocket(clientSocket);
+        std::cout << "Ошибка при получении имени файла" << std::endl;
+        return;
+    }
+
+    std::string fileName(buffer, bytesRecieved);
+
+    // Получим размер файла
+    bytesRecieved = recv(clientSocket, buffer, BUFFER_SIZE, 0);
+
+    if (bytesRecieved <= 0) {
+        closesocket(clientSocket);
+        std::cout << "Ошибка при получении размера файла" << std::endl;
+        return;
+    }
+
+    long fileSize = atol(buffer);
+
+    // Откроем файл для записи
+    std::ofstream file(fileName, std::ios::binary);
+
+    if (!file.is_open()) {
+        closesocket(clientSocket);
+        std::cout << "Ошибка при открытии файла для записи" << std::endl;
+        return;
+    }
+
+    // Получим данные файла
+    long totalBytesRecieved = 0;
+
+    while (totalBytesRecieved < fileSize) {
+        bytesRecieved = recv(clientSocket, buffer, BUFFER_SIZE, 0);
+
+        if (bytesRecieved <= 0) {
+            file.close();
+            closesocket(clientSocket);
+            std::cout << "Ошибка при получении данных файла" << std::endl;
+            return;
+        }
+
+        file.write(buffer, bytesRecieved);
+        totalBytesRecieved += bytesRecieved;
+    }
+
+    file.close();
+    std::cout << "Файл " << fileName << " успешно получен" << std::endl;
+
+    // Вернем подтверждение, что файл получен
+    const char* msg = "Файл получен!";
+    send(clientSocket, msg, strlen(msg), 0);
+
+    closesocket(clientSocket);
+}
+
+int main() {
+    setlocale(LC_ALL, "");
+
+    WSADATA wsaData;
+
+    // Иницифализация WinSock
+    int err = WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+    if (err != 0) {
+        std::cout << "Ошибка при инициализации WinSock." << std::endl;
+        return 1;
+    }
+
+    SOCKET serverSocket;
+
+    // Открытие сокета
+    serverSocket = socket(AF_INET, SOCK_STREAM, 0); // IPv4, TCP-протокол
+
+    if (serverSocket == INVALID_SOCKET) {
+        WSACleanup();
+        std::cout << "Ошибка при открытии сокета" << std::endl;
+        return 1;
+    }
+
+    // Ассоциирование сокета
+    sockaddr_in addr = { 0 };
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(1234);
+    addr.sin_addr.S_un.S_addr = htonl(INADDR_ANY); // Принимать сообщения на все интерфейсы
+
+    err = bind(serverSocket, (struct sockaddr*)&addr, sizeof(addr));
+
+    if (err == SOCKET_ERROR) {
+        closesocket(serverSocket);
+        WSACleanup();
+        std::cout << "Ошибка при привязке сокета" << std::endl;
+        return 1;
+    }
+
+    // Перевод сокета в режим прослушивания
+    err = listen(serverSocket, SOMAXCONN);
+
+    if (err == SOCKET_ERROR) {
+        closesocket(serverSocket);
+        WSACleanup();
+        std::cout << "Ошибка при переводе сокета в режим прослушивания" << std::endl;
+        return 1;
+    }
+
+    std::cout << "Сервер запущен и ожидает подключений..." << std::endl;
+
+    while (true) {
+        // Открываем соединения
+        SOCKET clientSocket = accept(serverSocket, nullptr, nullptr);
+
+        if (clientSocket == INVALID_SOCKET) {
+            std::cout << "Ошибка при открытии соединения" << std::endl;
+        }
+
+        // На каждое соединение создаем отдельный поток
+        std::thread(handleClient, clientSocket).detach();
+    }
+
+    shutdown(serverSocket, SD_BOTH);
+    closesocket(serverSocket);
+    WSACleanup();
+
+    return 0;
+}
